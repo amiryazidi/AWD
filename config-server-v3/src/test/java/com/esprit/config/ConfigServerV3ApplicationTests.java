a378a08f67759d6d7c0dd79f994c4000b88880e3
Amir <Amir.yazidi1@esprit.tn>
2021-10-24 03:55:43 +0100

    server-config

---
+++ config-server-v3/src/test/java/com/esprit/config/ConfigServerV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esprit.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
