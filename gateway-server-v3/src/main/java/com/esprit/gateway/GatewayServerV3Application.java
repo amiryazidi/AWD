ded8730d0e0820a4ac097be3048e00f2be3e5987
Amir <Amir.yazidi1@esprit.tn>
2021-10-24 17:14:44 +0100

    Zuul-filtres

--- gateway-server-v3/src/main/java/com/esprit/gateway/GatewayServerV3Application.java
+++ gateway-server-v3/src/main/java/com/esprit/gateway/GatewayServerV3Application.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.esprit.gateway.filters.*;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -12,6 +15,26 @@ public class GatewayServerV3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServerV3Application.class, args);
+	}
+	
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
+
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
 	}
 
 }
